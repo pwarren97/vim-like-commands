cef99e66949bee3bef212c59dae8920ebbe77653
pwarren97 <pwarren97@gmail.com>
2020-05-29 15:36:35 -0500

    Initial commit; alt-j (move down) and alt-k (move up) are working

---
+++ spec/vim-like-commands-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import VimLikeCommandsView from '../lib/vim-like-commands-view';
+
+describe('VimLikeCommandsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
